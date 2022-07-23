f9d8c0fed2513efe9187df68bc1fea2b61faf0ef
Toby <32206025+tobyabraham@users.noreply.github.com>
2022-07-23 16:14:55 +0100

    Update scraper v2.py

--- Scraper/scraper v2.py
+++ Scraper/scraper v2.py
@@ -1,16 +1,58 @@
-from bs4 import BeautifulSoup
-from requests_html import HTMLSession
-import requests
+#import requests
 
-url = 'https://www.jumia.com.ng/phones-tablets'
+import time
+
+import pandas as pd
+from requests_html import HTMLSession
+
+# products = r.html.xpath('//*[@id="maincontent"]/div/div[1]/div[4]', first=True) justfones
+# URL = 'https://www.jumia.com.ng/phones-tablets'
 
 s = HTMLSession()
-r = s.get(url)
+phonelist = []
 
-r.html.render(sleep=1)
+def request(url):
+    r = s.get(url)
+    r.html.render(sleep=1)
+    return r.html.xpath('//*[@id="jm"]/main/div[2]/div[3]/section/div[1]', first=True)
 
-products = r.html.xpath('//*[@id="jm"]/main/div[2]/div[3]/section/div[1]', first=True)
+def parse(products):
+    for item in products.absolute_links:
+        r = s.get(item)
+        try:
+            name = r.html.find('div.-df.-j-bet > div > h1', first=True).text
+            price = r.html.find('div.-hr.-mtxs.-pvs > div > span', first=True).text
+            link = r.html.find('head > meta:nth-child(7)', first=True).text
+        except:
+            name= 'none'
+            price= 'none'
+            link= 'none'
 
-for item in products.absolute_links:
-    r = s.get(item)
-    print(r.html.find('div.-fs0 -pls -prl', first=True).text)
+        phone = {
+            'name' : name,
+            'price' : price,
+            'link' : link
+        }
+        phonelist.append(phone)
+
+def output():
+    df = pd.DataFrame(phonelist)
+    df.to_csv('phonelist.csv')
+    print('Saved to csv file.')
+
+
+#https://www.jumia.com.ng/phones-tablets/?q=samsung%2F&page=3#catalog-listing
+x=1
+item = input("Enter search item:")
+while True:
+    try:
+        products = request(f'https://www.jumia.com.ng/catalog/?q={item}&page={x}#catalog-listing')
+        print('Getting items from page.')
+        parse(products)
+        print('Total Items: ', len(phonelist))
+        x=x+1
+        time.sleep(2)
+    except:
+        print('No more items!')
+        break
+output()    
